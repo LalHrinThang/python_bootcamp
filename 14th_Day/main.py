a21ea30f9c1d9079e3e34bd47d47fe821e5bb484
LalHrinThang <lalhrinthang@gmail.com>
2024-05-09 17:30:18 +0800

    FInal Checking

--- 14th_Day/main.py
+++ 14th_Day/main.py
@@ -3,14 +3,14 @@ import random
 
 from art import logo,vs
 from game_data import data
-
+import os
 
 # formatting data to print
 def format_data(comparison):
     comparison_name =comparison["name"]
     comaprison_desc = comparison["description"]
     comparison_country = comparison["country"]
-    return f"{comparison_name},a {comaprison_desc}, from {comparison_country}"
+    return f"{comparison_name}, a {comaprison_desc}, from {comparison_country}"
 
 # follower checker
 def follower_check(guess,a_followers,b_followers):
@@ -21,12 +21,14 @@ def follower_check(guess,a_followers,b_followers):
     
 score = 0
 continue_game = True
+comparison_A = random.choice(data)
+comparison_B = random.choice(data)
 # Todo : print random data for comparison from data
 print(logo)
 while continue_game:
-    comparison_A = random.choice(data)
+    comparison_A = comparison_B
     comparison_B = random.choice(data)
-    if comparison_A == comparison_B:
+    while comparison_A == comparison_B:
         comparison_B = random.choice(data)
 
     print(f"Compare A : {format_data(comparison_A)}")
@@ -41,16 +43,20 @@ while continue_game:
 
     a_followers = comparison_A["follower_count"]
     b_followers = comparison_B["follower_count"]
-
+    #print(f"A follower : {a_followers} / B follower : {b_followers}")
 # Todo : Check who has more followers => A or B 
     correct_ans = follower_check(guess,a_followers,b_followers)
 #print(correct_ans)
 
 # Todo : True =>add the score and print otherwise print only and end the game + adding while loop 
 
+# Clearing the Screen
+    os.system('clear')
+
+    print(logo)
     if correct_ans:
         score += 1
-        print(f"You're right, Current Score {score}")
+        print(f"You're right, Current Score : {score}")
     else:
         continue_game = False
-        print(f"Sorry,you're answer is wrong, Final Score{score}")
+        print(f"Sorry,your answer is wrong, \n Final Score : {score}")

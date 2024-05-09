1b6b8111de806d65538aac721fa98eae154e4968
LalHrinThang <lalhrinthang@gmail.com>
2024-05-09 17:10:39 +0800

    follower checker

--- 14th_Day/main.py
+++ 14th_Day/main.py
@@ -12,7 +12,12 @@ def format_data(comparison):
     comparison_country = comparison["country"]
     return f"{comparison_name},a {comaprison_desc}, from {comparison_country}"
 
-
+# follower checker
+def follower_check(guess,a_followers,b_followers):
+    if a_followers > b_followers:
+        return guess == "a"
+    else:
+        return guess == "b"
 # Todo : print random data for comparison from data
 print(logo)
 comparison_A = random.choice(data)
@@ -25,3 +30,14 @@ print(vs)
 
 
 print(f"Compare B : {format_data(comparison_B)}")
+
+# Todo : User Input
+
+guess = input("Who has more followers? Type 'A' or 'B': ").lower()
+
+a_followers = comparison_A["follower_count"]
+b_followers = comparison_B["follower_count"]
+
+# Todo : Check who has more followers => A or B 
+correct_ans = follower_check(guess,a_followers,b_followers)
+#print(correct_ans)

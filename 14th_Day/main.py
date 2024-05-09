3b527e3c0cd6e25a1ab26581ba5edcbdc995b211
LalHrinThang <lalhrinthang@gmail.com>
2024-05-09 17:17:32 +0800

    adding while loop and scores

--- 14th_Day/main.py
+++ 14th_Day/main.py
@@ -18,26 +18,39 @@ def follower_check(guess,a_followers,b_followers):
         return guess == "a"
     else:
         return guess == "b"
+    
+score = 0
+continue_game = True
 # Todo : print random data for comparison from data
 print(logo)
-comparison_A = random.choice(data)
-comparison_B = random.choice(data)
-if comparison_A == comparison_B:
+while continue_game:
+    comparison_A = random.choice(data)
     comparison_B = random.choice(data)
+    if comparison_A == comparison_B:
+        comparison_B = random.choice(data)
 
-print(f"Compare A : {format_data(comparison_A)}")
-print(vs)
+    print(f"Compare A : {format_data(comparison_A)}")
+    print(vs)
 
 
-print(f"Compare B : {format_data(comparison_B)}")
+    print(f"Compare B : {format_data(comparison_B)}")
 
 # Todo : User Input
 
-guess = input("Who has more followers? Type 'A' or 'B': ").lower()
+    guess = input("Who has more followers? Type 'A' or 'B': ").lower()
 
-a_followers = comparison_A["follower_count"]
-b_followers = comparison_B["follower_count"]
+    a_followers = comparison_A["follower_count"]
+    b_followers = comparison_B["follower_count"]
 
 # Todo : Check who has more followers => A or B 
-correct_ans = follower_check(guess,a_followers,b_followers)
+    correct_ans = follower_check(guess,a_followers,b_followers)
 #print(correct_ans)
+
+# Todo : True =>add the score and print otherwise print only and end the game + adding while loop 
+
+    if correct_ans:
+        score += 1
+        print(f"You're right, Current Score {score}")
+    else:
+        continue_game = False
+        print(f"Sorry,you're answer is wrong, Final Score{score}")
